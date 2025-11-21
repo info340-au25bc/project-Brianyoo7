7c18b3dc0b9c9713cac450396281ac6a7a322caa
tresan <tresan33@uw.edu>
2025-11-20 22:01:50 -0800

    added collections and viewcollections pages

---
+++ src/ViewCollection.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+
+const viewCollection = 
+        <section class="main-feed">
+            <button class="collection-button">Back</button>
+            <button class="collection-button">Edit</button>
+            <div class="collection-layout">
+                
+                <div class="collection-post">
+                    <div class="post-image-container">
+                        <img class="post-image" src="images/astronaut-career.jpg" alt="astronaut career switch"/>
+                    </div>
+                    <p class="post-title"><a href="post-card.html">My Success Story: Going From Doctor to Astronaut</a></p>
+                    <button class="collection-button">&#8593</button>
+                    <button class="collection-button">&#8595</button>
+                    <button class="collection-button">Remove</button>
+                    <button class="collection-button">Move to ...</button>
+                </div>
+
+                
+                <div class="collection-post">
+                    <div class="post-image-container">
+                        <img class="post-image" src="images/doctor-career.jpg" alt="medical doctor career switch"></img>
+                    </div>
+                    <p class="post-title">My Success Story: Going From SWE to Doctor</p>
+                    <button class="collection-button">&#8593</button>
+                    <button class="collection-button">&#8595</button>
+                    <button class="collection-button">Remove</button>
+                    <button class="collection-button">Move to ...</button>
+                </div>
+
+                
+                <div class="collection-post">
+                    <div class="post-image-container">
+                        <img class="post-image" src="images/software-engineer-career.jpg" alt="software engineer career switch"/>
+                    </div>
+                    <p class="post-title">My Success Story: Going From Sales to SWE</p>
+                    <button class="collection-button">&#8593</button>
+                    <button class="collection-button">&#8595</button>
+                    <button class="collection-button">Remove</button>
+                    <button class="collection-button">Move to ...</button>
+                </div>
+
+                
+                <div class="collection-post">
+                    <div class="post-image-container">
+                        <img class="post-image" src="images/engineer-career.jpg" alt="engineer career switch"/>
+                    </div>
+                    <p class="post-title">My Success Story: Going From Doctor to Engineer</p>
+                    <button class="collection-button">&#8593</button>
+                    <button class="collection-button">&#8595</button>
+                    <button class="collection-button">Remove</button>
+                    <button class="collection-button">Move to ...</button>
+                </div>
+
+                
+                <div class="collection-post">
+                    <div class="post-image-container">
+                        <img class="post-image" src="images/actor-career.gif" alt="actor career switch"/>
+                    </div>
+                    <p class="post-title">My Success Story: Going From Engineer to Actor</p>
+                    <button class="collection-button">&#8593</button>
+                    <button class="collection-button">&#8595</button>
+                    <button class="collection-button">Remove</button>
+                    <button class="collection-button">Move to ...</button>
+                </div>
+
+            
+                <div class="collection-post">
+                    <div class="post-image-container">
+                        <img class="post-image" src="images/talk-show-host-career.jpg" alt="talk show host career switch"/>
+                    </div>
+                    <p class="post-title">My Success Story: Going From Actor to Talk Show Host</p>
+                    <button class="collection-button">&#8593</button>
+                    <button class="collection-button">&#8595</button>
+                    <button class="collection-button">Remove</button>
+                    <button class="collection-button">Move to ...</button>
+                </div>
+
+            </div>
+        </section>;
